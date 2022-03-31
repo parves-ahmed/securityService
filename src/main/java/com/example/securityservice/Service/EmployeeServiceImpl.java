911c345835f95291bff6a48ebbc70586d2ad4467
parves <parves5405@gmail.com>
2022-03-31 11:16:59 +0600

    31-03-2022 add security config & filter

--- src/main/java/com/example/securityservice/Service/EmployeeServiceImpl.java
+++ src/main/java/com/example/securityservice/Service/EmployeeServiceImpl.java
@@ -5,17 +5,40 @@ import com.example.securityservice.Domain.Role;
 import com.example.securityservice.Repository.EmployeeRepository;
 import com.example.securityservice.Repository.RoleRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
-public class EmployeeServiceImpl implements EmployeeService {
+@Slf4j
+public class EmployeeServiceImpl implements EmployeeService, UserDetailsService {
     private final EmployeeRepository employeeRepository;
     private final RoleRepository roleRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String employeeName) throws UsernameNotFoundException {
+        Employee employee = employeeRepository.findEmployeesByEmployeeName(employeeName);
+        if(employee == null){
+            log.error("Employee not found in the database");
+            throw new UsernameNotFoundException("Employee not found in the database");
+        }
+        else {
+            log.info("Employee found in database: {}", employeeName);
+        }
+        Collection<SimpleGrantedAuthority>authorities = new ArrayList<>();
+        employee.getRoles().forEach(role -> authorities.add(new SimpleGrantedAuthority(role.getRoleName())));
+        return new org.springframework.security.core.userdetails.User(employee.getEmployeeName(), employee.getPassword(), authorities);
+    }
 
     @Override
     public Employee saveEmployee(Employee employee) {
@@ -43,4 +66,5 @@ public class EmployeeServiceImpl implements EmployeeService {
     public List<Employee> getEmployees() {
         return employeeRepository.findAll();
     }
+
 }
