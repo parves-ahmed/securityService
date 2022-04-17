be02a4c1238b586e690813a0dcc6191215e7a538
parves <parves5405@gmail.com>
2022-04-17 15:10:04 +0600

    17-04-2022 authorization and refresh token

--- src/main/java/com/example/securityservice/Security/SecurityConfig.java
+++ src/main/java/com/example/securityservice/Security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.securityservice.Security;
 
 import com.example.securityservice.Filter.CustomAuthenticationFilter;
+import com.example.securityservice.Filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +13,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.security.config.http.SessionCreationPolicy.*;
 
 @Configuration
@@ -29,10 +33,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
-        http.authorizeRequests().anyRequest().permitAll();
-        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+        http.authorizeRequests().antMatchers("/api/login/**", "/api/token/refresh").permitAll();
+        http.authorizeRequests().antMatchers(GET,"/api/employees/**").hasAnyAuthority("ROLE_USER");
+        http.authorizeRequests().antMatchers(POST,"/api/employee/**").hasAnyAuthority("ROLE_ADMIN");
+        http.authorizeRequests().anyRequest().authenticated();
+        http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
--- src/main/java/com/example/securityservice/api/EmployeeResource.java
+++ src/main/java/com/example/securityservice/api/EmployeeResource.java
@@ -1,16 +1,32 @@
 package com.example.securityservice.api;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.example.securityservice.Domain.Employee;
 import com.example.securityservice.Domain.Role;
 import com.example.securityservice.Service.EmployeeService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.net.URI;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("/api")
@@ -23,13 +39,13 @@ public class EmployeeResource {
         return ResponseEntity.ok().body(employeeService.getEmployees());
     }
 
-    @PostMapping("/save/employee")
+    @PostMapping("/employee/save")
     public ResponseEntity<Employee>saveEmployee(@RequestBody Employee employee){
         URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/save/employee").toUriString());
         return ResponseEntity.created(uri).body(employeeService.saveEmployee(employee));
     }
 
-    @PostMapping("/save/role")
+    @PostMapping("/role/save")
     public ResponseEntity<Role>saveRole(@RequestBody Role role){
         URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/save/role").toUriString());
         return ResponseEntity.created(null).body(employeeService.saveRole(role));
@@ -39,6 +55,42 @@ public class EmployeeResource {
     public ResponseEntity<?>addRoleToUser(@RequestBody RoleToEmployeeForm form){
         employeeService.addRoleToEmployee(form.getEmployeeName(), form.getRoleName());
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/token/refresh")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String authorizationHeader = request.getHeader(AUTHORIZATION);
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            try {
+
+                String refresh_token = authorizationHeader.substring("Bearer ".length());
+                Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+                JWTVerifier verifier = JWT.require(algorithm).build();
+                DecodedJWT decodedJWT = verifier.verify(refresh_token);
+                String username = decodedJWT.getSubject();
+                Employee employee = employeeService.getEmployee(username);
+                String access_token = JWT.create()
+                        .withSubject(employee.getEmployeeName())
+                        .withExpiresAt(new Date(System.currentTimeMillis() + 40 * 60 * 1000))
+                        .withIssuer(request.getRequestURL().toString())
+                        .withClaim("roles", employee.getRoles().stream().map(Role::getRoleName).collect(Collectors.toList()))
+                        .sign(algorithm);
+                Map<String, String> tokens = new HashMap<>();
+                tokens.put("access_token", access_token);
+                response.setContentType(APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), tokens);
+            } catch (Exception exception) {
+                response.setHeader("error", exception.getMessage());
+                response.setStatus(FORBIDDEN.value());
+                Map<String, String> error = new HashMap<>();
+                error.put("error", exception.getMessage());
+                response.setContentType(APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), error);
+            }
+        }
+        else {
+            throw new RuntimeException("Refresh token is missing");
+        }
     }
 }
 
