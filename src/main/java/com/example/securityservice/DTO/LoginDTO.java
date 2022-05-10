182d85a04ac5f2422583a968ffe5bcb44cd6bdc4
parves <parves5405@gmail.com>
2022-05-10 12:06:45 +0600

    10-05-2022 add cors configuration

---
+++ src/main/java/com/example/securityservice/DTO/LoginDTO.java
@@ -0,0 +1,13 @@
+package com.example.securityservice.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginDTO {
+    String userName;
+    String password;
+}
